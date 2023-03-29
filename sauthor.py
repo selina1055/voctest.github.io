d8861d20d1506936712c00709e09616d3865cd3c
selina1055 <73380732+selina1055@users.noreply.github.com>
2023-03-29 18:54:23 +0800

    Delete sauthor.py

--- sauthor.py
+++
@@ -1,9 +0,0 @@
-class data():
-    def __init__(self):
-        self.menu_title = '小說查詢'
-        self.account = ''
-        self.menu = {
-            'a':'個人資料查詢',
-            'b':'選擇類型設定',
-            'c':'小說建檔查詢',
-            'd':'排行榜統計',}
